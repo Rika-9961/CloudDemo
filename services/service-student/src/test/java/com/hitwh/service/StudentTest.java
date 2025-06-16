52b40673afdb82068222e9488822cf217d087c9e
John Six <1424157706@qq.com>
2025-06-16 13:03:22 +0800

    1.1:非跨微服务成果

---
+++ services/service-student/src/test/java/com/hitwh/service/StudentTest.java
@@ -0,0 +1,20 @@
+package com.hitwh.service;
+
+import com.hitwh.dto.pojo.StudentDTO;
+import com.hitwh.entity.enumeration.EducationBackgroundType;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class StudentTest {
+//    @Resource
+//    private StudentService studentService;
+//
+//    @Test
+//    void test(){
+//        // 测试添加学员
+//        StudentDTO studentDTO = new StudentDTO(2L, EducationBackgroundType.BACHELOR, "张三", "男", "20230001", "12345678901", 1L);
+//        studentService.addStudent(studentDTO);
+//    }
+}
